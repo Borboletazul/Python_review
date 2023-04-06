5ac395e96e9894e0fa8311c1cc024b59429d99d4
Borboletazul <129607009+Borboletazul@users.noreply.github.com>
2023-04-06 02:16:20 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Calculo IMC")
+a = float(input("digite sua altura em metros(ex.:1.70):"))
+p = float(input("digite seu peso em Kg:"))
+IMC = p/a**2
+print("IMC:",IMC)
+if IMC<20.7:
+  print("Condição: Abaixo do peso")
+elif 20.7<=IMC<=27.8:
+  print("Condição: Peso normal")
+elif 27.8<IMC<=31.1:
+  print("Condição: Acima do peso")
+else:
+  print("Condição: Obesidade")
