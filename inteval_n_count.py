85fd7f11449405c2abcb76a1f9e986e1b6251770
Borboletazul <129607009+Borboletazul@users.noreply.github.com>
2023-04-07 00:26:43 -0300

    Update inteval_n_count.py

--- inteval_n_count.py
+++ inteval_n_count.py
@@ -1,9 +1,12 @@
-print("Contagem de números em intervalos predeterminados.\n\nDigite números inteiros e pressione enter.\nO programa aceitará entradas até receber o número 0\n")
 n=1
 i1=0
 i2=0
 i3=0
 i4=0
+
+print("Contagem de números em intervalos predeterminados.")
+print("\nDigite números inteiros e pressione enter.")
+print("O programa aceitará entradas até receber o número 0\n")
 
 while n > 0:
   n=int(input("digite um número:"))
@@ -14,12 +17,13 @@ while n > 0:
   elif n >=26 and n <= 50 :
     i2 = i2 + 1
 
-
   elif n >=51 and n <= 75 :
     i3 = i3 + 1
 
   elif n >=76 and n <= 100 :
     i4 = i4 + 1
 
-
-print("\nA quantidade de números no intervalo [0,25] é:",i1,"\nA quantidade de números no intervalo [26,50] é:",i2,"\nA quantidade de números no intervalo [51,75] é:",i3,"\nA quantidade de números no intervalo [76,100] é:",i4)
+print("\nA quantidade de números no intervalo [0,25] é:",i1)
+print("A quantidade de números no intervalo [26,50] é:",i2)
+print("\nA quantidade de números no intervalo [51,75] é:",i3)
+print("\nA quantidade de números no intervalo [76,100] é:",i4)
