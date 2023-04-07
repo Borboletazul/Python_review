41f1f0aa96b68f12a54459a5d8db1eac31c6bf71
Borboletazul <129607009+Borboletazul@users.noreply.github.com>
2023-04-06 23:29:30 -0300

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,20 @@
+print("Programa: professor LUAN GAMEPLAYS")
+
+nota=int(input("Digita tua nota:"))
+
+if nota < 11 :
+  print("essa é tua nota meu nobre ? ")
+  if nota < 7 :
+    print("nota baixa né...vai estudar mizerávi")
+
+    if nota < 5 :
+      print("tapoxa menó, cria vergonha na cara zé pingélo")
+
+  elif nota == 7 :
+      print("na bucha hein...")
+
+  elif nota > 7:
+   print("qual foi menor kkkk, largou o frifaire, BOA!!!" )
+
+   if nota > 8 :
+    print("tu é brabo meu nobre.")
